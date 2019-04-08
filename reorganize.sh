fb145ebccd9af047db1de587a00b1e28988bd377
Jan Sowiński <sowinj@gmail.com>
2019-04-08 15:44:59 +0200

    Lefto only star

--- reorganize.sh
+++ reorganize.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 mkdir /tmp/ngx-trewviewer-tmp
-mv ./{.,}* /tmp/ngx-trewviewer-tmp
+mv * /tmp/ngx-trewviewer-tmp
 mv /tmp/ngx-trewviewer-tmp/dist/src/* ./
 mv /tmp/ngx-trewviewer-tmp source
